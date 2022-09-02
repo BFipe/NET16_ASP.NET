0c35f836dbb980c2bf43c698a140fb5134b50a59
BFipe <ilya.maximov11@gmail.com>
2022-09-02 09:28:26 +0300

    Homework 13

---
+++ Homework 13/Homework 13/Controllers/BookController.cs
@@ -0,0 +1,14 @@
+﻿using Homework_13.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_13.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            BookViewModel book = new BookViewModel("Azbuka", "Ilya", 54);
+            return View(book);
+        }
+    }
+}
---
+++ Homework 13/Homework 13/Models/BookViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework_13.Models
+{
+    public class BookViewModel
+    {
+        public BookViewModel(string name, string author, int pages)
+        {
+           Name = name;
+           Author = author;    
+           Pages = pages;
+        }
+            
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+    }
+}
