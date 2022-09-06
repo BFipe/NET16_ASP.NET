6bc31c641c54a29f7916b27cfd656b27270f2007
BFipe <ilya.maximov11@gmail.com>
2022-09-06 11:51:50 +0300

    Homework 14

---
+++ Homework 14/Homework 14/Controllers/VetController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Homework_14.Models;
+using System;
+
+namespace Homework_14.Controllers
+{
+    public class VetController : Controller
+    {
+        [HttpGet]
+        public IActionResult VetApplication()
+        {
+            ModelState.Clear();
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult VetApplication(DogViewModel dog)
+        {
+            Console.WriteLine($"Pet name: {dog.PetName}, age: {dog.PetAge}, weight: {dog.PetWeight}, owner: {dog.OwnerName}");
+            return VetApplication();
+        }
+    }
+}
+
---
+++ Homework 14/Homework 14/Models/DogViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework_14.Models
+{
+    public class DogViewModel
+    {
+        public DogViewModel()
+        {
+
+        }
+
+        public string PetName { get; set; }
+        public string OwnerName { get; set; }
+        public int PetAge { get; set; }
+        public int PetWeight { get; set; }
+    }
+}
